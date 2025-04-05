58d2fe6e3796239c7d4f8347ff66ae5c19d945eb
MachiThomas <mthomas8865@conestogac.on.ca>
2025-04-05 18:33:56 -0400

    technical E

---
+++ classifiers.cpp
@@ -0,0 +1,13 @@
+
+#include <cstdio>
+
+void train() {
+    printf("train\n");
+    printf("The first feature is added\n");
+
+}
+
+void predict() {
+    printf("predict\n");
+ printf("The first feature is added\n");
+}
