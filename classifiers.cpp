926ec22f2f6edc733f6615d7a3e78b97896bc962
MachiThomas <mthomas8865@conestogac.on.ca>
2025-04-05 20:35:13 -0400

    Added second feature to train function

---
+++ classifiers.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+void train() {
+    printf("train\n");
+    printf("The first feature is added\n");
+    printf(“The second feature is added”);
+}
+
+void predict() {
+    printf("predict\n");
+    printf("The first feature is added\n");
+    printf(“The second feature is added”);
+}
